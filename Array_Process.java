cc09ba125f21546e079ae743c74abad7090133d9
trashcanbug <155243907+trashcanbug@users.noreply.github.com>
2025-07-11 20:24:05 +0530

    Add files via upload

---
+++ Array_Process.java
@@ -0,0 +1,11 @@
+public class Array_Process {
+    public static void main(String[] args) {
+int[] numbers = {5, 2, 9, 1, 5};
+int sum = 0;
+for(int n : numbers) {
+sum += n;
+}
+
+System.out.println("Array sum: " + sum);
+}
+}
---
+++ temp.java
@@ -0,0 +1,7 @@
+public class temp {
+       public static void main(String[] args) { 
+        double fahrenheit = 98.6; 
+        double celsius = (fahrenheit - 32) * 5/9; 
+        System.out.printf("%.2f°F = %.2f°C\n", fahrenheit, celsius); 
+    } 
+}
